6b374b343321e7376808810565c9c50194414cfa
NoDocCat <nodoccat@outlook.com>
2023-03-06 20:29:47 +0800

    build: 更新 gradle plugin 版本和构建目标

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.7.20"
-  id("org.jetbrains.intellij") version "1.10.1"
+  id("org.jetbrains.kotlin.jvm") version "1.8.10"
+  id("org.jetbrains.intellij") version "1.13.1"
 }
 
 group = "sh.catx"
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2022.1.4")
+  version.set("2022.3.2")
   type.set("IC") // Target IDE Platform
 
   plugins.set(listOf(/* Plugin Dependencies */))
@@ -23,16 +23,16 @@ intellij {
 tasks {
   // Set the JVM compatibility versions
   withType<JavaCompile> {
-    sourceCompatibility = "11"
-    targetCompatibility = "11"
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
   }
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
   }
 
   patchPluginXml {
-    sinceBuild.set("221")
-    untilBuild.set("231.*")
+    sinceBuild.set("223")
+    untilBuild.set("223.*")
   }
 
   signPlugin {
