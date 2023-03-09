e0457dd25234e52643ce9ee81727c8ac4af71f45
NoDocCat <nodoccat@outlook.com>
2023-03-09 20:55:28 +0800

    feat: 完成许可证生成

---
+++ src/main/kotlin/sh/catx/chooselicense/license/License.kt
@@ -0,0 +1,27 @@
+package sh.catx.chooselicense.license
+
+val LICENSE_LIST = listOf(
+  License("MIT", "[fullname]", "[year]"),
+  License("AGPL-3.0", "<name of author>", "<year>"),
+)
+
+class License(
+  val type: String,
+  private val authorTag: String?,
+  private val yearTag: String?,
+) {
+  fun generate(author: String, year: String): String {
+    val templateFile = this.javaClass.getResource("/license/$type")
+    var template = templateFile?.readText() ?: ""
+
+    if (authorTag != null) {
+      template = template.replace(authorTag, author)
+    }
+
+    if (yearTag != null) {
+      template = template.replace(yearTag, year)
+    }
+
+    return template
+  }
+}
