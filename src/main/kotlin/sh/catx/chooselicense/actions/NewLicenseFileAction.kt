ae6e2054f644f0fe6ae817d669c3027dcc858bfd
NoDocCat <nodoccat@outlook.com>
2023-03-30 21:05:46 +0800

    feat: 完成内容生成和文件写入

--- src/main/kotlin/sh/catx/chooselicense/actions/NewLicenseFileAction.kt
+++ src/main/kotlin/sh/catx/chooselicense/actions/NewLicenseFileAction.kt
@@ -1,10 +1,36 @@
 package sh.catx.chooselicense.actions
 
 import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import sh.catx.chooselicense.ui.NewLicenseFileDialog
 
 class NewLicenseFileAction : AnAction() {
   // action 触发点
   override fun actionPerformed(e: AnActionEvent) {
-    println("actionPerformed")
+    val dialog = NewLicenseFileDialog()
+    val view = e.getRequiredData(LangDataKeys.IDE_VIEW)
+    val directory = view.orChooseDirectory ?: return
+
+    if (dialog.showAndGet()) {
+      val fileName = dialog.fileName
+      val content = dialog.license.generate(dialog.author, dialog.year)
+      val file = createFile(directory, fileName, content)
+      view.selectElement(file)
+    }
+  }
+
+  private fun createFile(directory: PsiDirectory, fileName: String, content: String): PsiFile {
+    return runWriteAction {
+      var file = directory.findFile(fileName)
+
+      if (file == null) {
+        file = directory.createFile(fileName)
+      }
+
+      file.virtualFile.setBinaryContent(content.toByteArray())
+      file
+    }
   }
 }
