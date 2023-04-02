4004b3f64da3284170202d5c46b7dfc58ff8cf0d
NoDocCat <nodoccat@outlook.com>
2023-04-02 23:35:38 +0800

    feat: 添加常用许可证模板

--- src/main/kotlin/sh/catx/chooselicense/license/License.kt
+++ src/main/kotlin/sh/catx/chooselicense/license/License.kt
@@ -3,6 +3,16 @@ package sh.catx.chooselicense.license
 val LICENSE_LIST = listOf(
   License("MIT", "[fullname]", "[year]"),
   License("AGPL-3.0", "<name of author>", "<year>"),
+  License("Apache-2.0", "[name of copyright owner]", "[yyyy]"),
+  License("BSD-2-Clause", "[fullname]", "[year]"),
+  License("BSD-3-Clause", "[fullname]", "[year]"),
+  License("BSD-3-Clause-Clear", "[fullname]", "[year]"),
+  License("BSD-4-Clause", "[fullname]", "[year]"),
+  License("GPL-2.0", "<name of author>", "<year>"),
+  License("GPL-3.0", "<name of author>", "<year>"),
+  License("LGPL-2.1", "<name of author>", "<year>"),
+  License("LGPL-3.0", null, null),
+  License("MPL-2.0", null, null),
 )
 
 class License(
