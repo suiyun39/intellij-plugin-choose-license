351aebc4cad2dc44317857f4f67582c10c9518c7
NoDocCat <nodoccat@outlook.com>
2023-04-06 21:38:49 +0800

    build: 更新 kotlin 和 intellij 插件

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.8.10"
-  id("org.jetbrains.intellij") version "1.13.1"
+  id("org.jetbrains.kotlin.jvm") version "1.8.20"
+  id("org.jetbrains.intellij") version "1.13.3"
 }
 
 group = "sh.catx"
