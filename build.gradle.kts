97acecd10dfd70e0ffb69588f9a7221a5977605f
NoDocCat <nodoccat@outlook.com>
2023-04-06 21:40:28 +0800

    build: 调整构建目标, 不再要求最高版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2022.3.2")
+  version.set("2023.1")
   type.set("IC") // Target IDE Platform
 
   plugins.set(listOf(/* Plugin Dependencies */))
@@ -32,7 +32,7 @@ tasks {
 
   patchPluginXml {
     sinceBuild.set("223")
-    untilBuild.set("223.*")
+//    untilBuild.set("223.*")
   }
 
   signPlugin {
