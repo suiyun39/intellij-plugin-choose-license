e9dcf54404859979b7030ad9d94757a1c3e64776
suiyun39 <suiyun39@gmail.com>
2024-01-09 00:34:41 +0800

    build: 优化构建配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,44 +4,37 @@ plugins {
   id("org.jetbrains.intellij") version "1.16.1"
 }
 
-group = "sh.catx"
+group = "sh.suiyun"
 version = "1.0.1"
 
 repositories {
   mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+// https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.1")
-  type.set("IC") // Target IDE Platform
-
-  plugins.set(listOf(/* Plugin Dependencies */))
+  version = "2023.1"
+  type = "IC"
 }
 
 tasks {
-  // Set the JVM compatibility versions
   withType<JavaCompile> {
     sourceCompatibility = "17"
     targetCompatibility = "17"
   }
+
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
   }
 
+  // 配置插件的兼容性范围
+  // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#platformVersions
   patchPluginXml {
-    sinceBuild.set("223")
-//    untilBuild.set("223.*")
+    sinceBuild = "231"
   }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
-
+  // 插件发布
   publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
+    token = System.getenv("PUBLISH_TOKEN")
   }
 }
