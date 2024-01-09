46d472a8375352a68f3d2ae80d2fea55027b00b8
suiyun39 <suiyun39@gmail.com>
2024-01-09 23:46:04 +0800

    feat: 完成必填校验和基本的输入项

--- src/main/kotlin/sh/suiyun/chooseLicense/ui/NewLicenseFileDialog.kt
+++ src/main/kotlin/sh/suiyun/chooseLicense/ui/NewLicenseFileDialog.kt
@@ -1,14 +1,35 @@
 package sh.suiyun.chooseLicense.ui
 
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.dsl.builder.*
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import javax.swing.JComponent
+import sh.suiyun.chooseLicense.utils.Storage
+
+/**
+ * 扩展 Cell<JBTextField>, 增加必填校验
+ */
+fun Cell<JBTextField>.validationRequired(fieldName: String): Cell<JBTextField> {
+  return validationOnInput {
+    if (it.text.isNullOrBlank()) {
+      ValidationInfo("$fieldName is required", it)
+    } else {
+      null
+    }
+  }
+}
 
 /**
  * 新建 License 文件对话框
  */
 class NewLicenseFileDialog : DialogWrapper(true) {
+  var author = Storage.author
+  var year = Storage.year
+  var fileName = Storage.fileName
+
   init {
     title = "New License File"
 
@@ -19,8 +40,18 @@ class NewLicenseFileDialog : DialogWrapper(true) {
   /**
    * 创建对话框内容
    */
-  override fun createCenterPanel(): JComponent? {
-    return null
+  override fun createCenterPanel(): JComponent {
+    return panel {
+      row("Author:") {
+        textField().bindText(::author).validationRequired("Author").align(Align.FILL)
+      }
+      row("Year:") {
+        textField().bindText(::year).validationRequired("Year").align(Align.FILL)
+      }
+      row("FileName:") {
+        textField().bindText(::fileName).validationRequired("FileName").align(Align.FILL)
+      }
+    }
   }
 
   /**
