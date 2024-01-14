ae88492b36de4a6446953186684ec6b9bb1495fe
suiyun39 <suiyun39@gmail.com>
2024-01-15 07:36:54 +0800

    feat: 完成 license 选择框

--- src/main/kotlin/sh/suiyun/chooseLicense/ui/LicenseCombo.kt
+++ src/main/kotlin/sh/suiyun/chooseLicense/ui/LicenseCombo.kt
@@ -1,11 +1,16 @@
 package sh.suiyun.chooseLicense.ui
 
 import com.intellij.openapi.ui.ComboBox
+import javax.swing.DefaultComboBoxModel
+import sh.suiyun.chooseLicense.license.License
+import sh.suiyun.chooseLicense.license.LICENSE_LIST
 
 /**
  * LICENSE 选择框
- * 参考: com.intellij.application.options.editor.fonts.FontFamilyCombo.java
- * todo: 此处 ComboBox 的泛型待定, 待确定 LICENSE 的数据结构后再确定
  */
-class LicenseCombo : ComboBox<String>() {
+class LicenseCombo : ComboBox<License>() {
+  init {
+    model = DefaultComboBoxModel(LICENSE_LIST.toTypedArray())
+    isSwingPopup = false
+  }
 }
