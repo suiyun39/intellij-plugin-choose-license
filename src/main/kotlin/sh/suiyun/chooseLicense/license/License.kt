5ff41cc454e7b1256f25fc6d5353bab66b711307
suiyun39 <suiyun39@gmail.com>
2024-01-15 08:45:14 +0800

    feat: 合并生成方法, 补充缺失的规则

--- src/main/kotlin/sh/suiyun/chooseLicense/license/License.kt
+++ src/main/kotlin/sh/suiyun/chooseLicense/license/License.kt
@@ -1,60 +1,39 @@
 package sh.suiyun.chooseLicense.license
 
 class License(val spdxId: String, val content: String) {
-  private fun replaceAuthor(author: String, text: String): String {
-    when (spdxId) {
-      "AGPL-3.0", "GPL-2.0", "GPL-3.0", "LGPL-2.1" -> {
-        return text.replace("<name of author>", author)
-      }
-
-      "Apache-2.0" -> {
-        return text.replace("[name of copyright owner]", author)
-      }
-
-      "BSD-2-Clause", "BSD-3-Clause", "BSD-3-Clause-Clear", "BSD-4-Clause", "MIT", "MIT-0", "ISC" -> {
-        return text.replace("[fullname]", author)
-      }
-
-      "WTFPL" -> {
-        return text.replace("Sam Hocevar <sam@hocevar.net>", author)
-      }
-
-      else -> {
-        return text
-      }
-    }
-  }
-
-  private fun replaceYear(year: String, text: String): String {
-    when (spdxId) {
-      "AGPL-3.0", "GPL-2.0", "GPL-3.0", "LGPL-2.1" -> {
-        return text.replace("<year>", year)
-      }
-
-      "Apache-2.0" -> {
-        return text.replace("[yyyy]", year)
-      }
-
-      "BSD-2-Clause", "BSD-3-Clause", "BSD-3-Clause-Clear", "BSD-4-Clause", "MIT", "MIT-0", "ISC" -> {
-        return text.replace("[year]", year)
-      }
-
-      "WTFPL" -> {
-        return text.replace("Copyright (C) 2004", "Copyright (C) $year")
-      }
-
-      else -> {
-        return text
-      }
-    }
-  }
-
   /**
    * 生成 license
    */
   fun generateLicense(author: String, year: String): String {
-    val text = replaceAuthor(author, content)
-    return replaceYear(year, text)
+    when (spdxId) {
+      "AGPL-3.0", "GPL-2.0", "GPL-3.0", "LGPL-2.1" -> {
+        return content
+          .replace("<name of author>", author)
+          .replace("<year>", year)
+      }
+
+      "Apache-2.0" -> {
+        return content
+          .replace("[name of copyright owner]", author)
+          .replace("[yyyy]", year)
+      }
+
+      "0BSD", "BSD-2-Clause", "BSD-3-Clause", "BSD-3-Clause-Clear", "BSD-4-Clause", "MIT", "MIT-0", "ISC" -> {
+        return content
+          .replace("[fullname]", author)
+          .replace("[year]", year)
+      }
+
+      "WTFPL" -> {
+        return content
+          .replace("Sam Hocevar <sam@hocevar.net>", author)
+          .replace("Copyright (C) 2004", "Copyright (C) $year")
+      }
+
+      else -> {
+        return content
+      }
+    }
   }
 
   override fun toString(): String {
